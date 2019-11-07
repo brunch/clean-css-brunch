b579474823e7f0f53b2bc27945a0a6c8c41af65b
DarkRoku12 <enmarey2012@hotmail.com>
2019-11-07 01:20:50 -0400

    Bump clean-css version & add .map files support for minified css files

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
-'use strict';
 
-const CleanCSS = require('clean-css');
+const CleanCSS = require( "clean-css" ) ;
 
-class CleanCSSMinifier {
-  constructor(config) {
-    this.options = config && config.plugins && config.plugins.cleancss || {};
+class CleanCSSMinifier 
+{
+  constructor( config ) 
+  {
+    this.options = config && config.plugins && config.plugins.cleancss || {} ;
   }
 
-  optimize(file) {
-    const data = file.data;
-    const path = file.path;
+  optimize( file ) 
+  {
+    const data = file.data ;
+    const path = file.path ;
 
-    try {
-      if (this.options.ignored && this.options.ignored.test(path)) {
+    try
+    {
+      if( this.options.ignored && this.options.ignored.test( path ) ) 
+      {
         // ignored file path: return non minified
         return Promise.resolve(data);
       }
-    } catch (e) {
+    } 
+    catch( e ) 
+    {
       return Promise.reject(`error checking ignored files to minify ${e}`);
     }
 
-    try {
-      const min = new CleanCSS(this.options).minify(data);
-      return Promise.resolve(min.styles || data);
-    } catch (error) {
+    try 
+    {
+      let prevMap = JSON.parse( file.map.toString() ) ; 
+      let min     = new CleanCSS( this.options ).minify( data , prevMap ) ; 
+      let currMap = JSON.parse( min.sourceMap.toString() ) ;
+      let newMap  = Object.assign( prevMap , currMap ) ;
+      let nFile   = 
+      {
+        data : min.styles ,
+        map  : newMap ,
+      };
+
+      let stdinIdx = nFile.map.sources.indexOf( "$stdin" ) ;
+
+      if( stdinIdx != -1 )
+      {
+        newMap.sourcesContent.splice( stdinIdx , 0 , "/* silence is golden */" ) ;
+      }
+
+      return Promise.resolve( nFile ) ;
+    } 
+    catch( error ) 
+    {
       return Promise.reject(`CSS minify failed on ${path}: ${error}`);
     }
   }
 }
 
-CleanCSSMinifier.prototype.brunchPlugin = true;
-CleanCSSMinifier.prototype.type = 'stylesheet';
+CleanCSSMinifier.prototype.brunchPlugin = true ;
+CleanCSSMinifier.prototype.extension = "css" ;
+CleanCSSMinifier.prototype.type = "stylesheet" ;
 
 module.exports = CleanCSSMinifier;
